22849b6444c0e78785ac3b033390e24fd8aa5ba1
박소현 <park.sohyun0430@gmail.com>
2023-07-25 23:35:02 +0900

    [S09P12B205-92] Feat: Member Entity 생성

--- Back-End/manna/src/main/java/com/ssafy/manna/domain/member/domain/Member.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/domain/member/domain/Member.java
@@ -1,13 +1,24 @@
 package com.ssafy.manna.domain.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
 
     @Id
     @GeneratedValue
-    private Long id;
+    private String id;
+    private String pwd;
+    private String name;
+    private String gender;
+    private String role;
+    @Column(name = "crated_date")
+    private LocalDateTime createdDate;
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+
 }
