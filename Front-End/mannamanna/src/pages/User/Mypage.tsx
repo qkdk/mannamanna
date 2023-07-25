16cfb4b6219c428275389345a59031736d70ebe9
노윤식 <gozldss12345@naver.com>
2023-07-26 01:16:17 +0900

    [S09P12B205-77] WIP: 파일 오류 수정 및 로그인 제작

--- Front-End/mannamanna/src/pages/User/Mypage.tsx
+++ Front-End/mannamanna/src/pages/User/Mypage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components'
-import MacBookBox from '../../components/common/MacbookBox';
 import Profile from '../../components/common/Profile';
 import Button from '@mui/material/Button';
 
