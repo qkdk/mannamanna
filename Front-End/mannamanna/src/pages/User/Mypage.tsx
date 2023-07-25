bea7a0a26021c0d74c84d5f5ef00b6fe6a37f856
홍영기 <hongyk9885@gmail.com>
2023-07-25 21:32:42 +0900

    [S09P12B205-79] WIP: 0725 마이페이지 제작

--- Front-End/mannamanna/src/pages/User/Mypage.tsx
+++ Front-End/mannamanna/src/pages/User/Mypage.tsx
@@ -1,9 +1,59 @@
- import React from 'react';
- 
+import React from 'react';
+import styled from 'styled-components'
+import MacBookBox from '../../components/common/MacbookBox';
+import Profile from '../../components/common/Profile';
+import Button from '@mui/material/Button';
+
+let Hello = styled.div`
+ border: solid 7px black;
+  border-radius:40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ` 
+type MyPageButtonProps = {
+    children: string;
+};
+  
+const MyPageButton = ({ children }: MyPageButtonProps) => {
+    return(
+        <Button
+        sx={{
+            m : 1,
+            color: "#black",
+            borderColor: "ffcced",
+        }}
+        variant="contained"
+        >{children}</Button>
+    )
+}
+
+
+
+
  const Mypage = () => {
     return (
-        <div>
-            마이페이지
+        <div style={{width:'100%',height:'90vh' ,display:'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+            <Hello style={{width:'30%',height:'80%'}}>
+                <Profile/>
+                <br/>
+                안녕
+                <br/>
+                <Button variant="outlined">내 정보 수정</Button>
+                <br/>
+                <Button variant="outlined">내 정보 수정</Button>
+                <br/>
+                <Button variant="outlined">내 정보 수정</Button>
+                <br/>
+                <Button variant="outlined">내 정보 수정</Button>
+                <br/>
+                <MyPageButton>안녕</MyPageButton>
+                마일리지 : 10000
+            </Hello>
+            <Hello style={{width:'50%',height:'80%'}}>
+                가나다라
+            </Hello>
         </div>
     );
  };
