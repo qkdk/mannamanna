13e514e61f8f11770b330b266c63ee4e67415490
JaeiKim <dobbykim0320@gmail.com>
2023-07-27 02:26:05 +0900

    [S09P12B205-97] WIP : 소개팅 추천 페이지 구체화 작업

--- Front-End/mannamanna/src/components/common/Sogeting/FilterBody.tsx
+++ Front-End/mannamanna/src/components/common/Sogeting/FilterBody.tsx
@@ -1,43 +1,55 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import SelectionObj1 from "./Selection/Selection1";
+import SelectionObj2 from "./Selection/Selection2";
+import SelectionObj3 from "./Selection/Selection3";
+import SelectionObj4 from "./Selection/Selection4";
 
-const FilterBody = styled.div `
-height: 55vh;
-width: 12vw;
-border: solid black 0.4rem;
-// background-color: white;
-border-radius: 5vh;
-`
-const UpBar = styled.div `
-height: 3.5vh;
-width: 12vw;
-background-color: pink;
-border-top-right-radius: 5vw;
-border-top-left-radius:  5vw;
-border-bottom: solid black 0.4rem;
-`
+const FilterBody = styled.div`
+  height: 55vh;
+  width: 12vw;
+  border: solid black 0.4rem;
+  // background-color: white;
+  border-radius: 5vh;
+`;
+const UpBar = styled.div`
+  height: 3.5vh;
+  width: 12vw;
+  background-color: pink;
+  border-top-right-radius: 5vw;
+  border-top-left-radius: 5vw;
+  border-bottom: solid black 0.4rem;
+`;
 
-const Box = styled.div`
-height: 46.7vh;
-width: 12vw;
-`
+const Box = styled.form`
+  height: 46.7vh;
+  width: 12vw;
+  display: flex;
+  flex-direction: column;
+`;
 
-const DownBar = styled.div `
-height: 3.5vh;
-width: 12vw;
-background-color: pink;
-border-bottom-right-radius: 5vw;
-border-bottom-left-radius:  5vw;
-border-top: solid black 0.4rem;
-`
+const DownBar = styled.div`
+  height: 3.5vh;
+  width: 12vw;
+  background-color: pink;
+  border-bottom-right-radius: 5vw;
+  border-bottom-left-radius: 5vw;
+  border-top: solid black 0.4rem;
+`;
 
 const Filter = () => {
-    return (<FilterBody>
-        <UpBar></UpBar>
-        <Box></Box>
-        <DownBar></DownBar>
+  return (
+    <FilterBody>
+      <UpBar></UpBar>
+      <Box>
+        <SelectionObj1 />
+        <SelectionObj2 />
+        <SelectionObj3 />
+        <SelectionObj4 />
+      </Box>
+      <DownBar></DownBar>
     </FilterBody>
-    );
-}
+  );
+};
 
 export default Filter;
--- Front-End/mannamanna/src/pages/Sogaeting/SoagetingMain.tsx
+++ Front-End/mannamanna/src/pages/Sogaeting/SoagetingMain.tsx
@@ -1,95 +1,175 @@
-import React from 'react';
-import Back from '../../components/common/Sogeting/SogetingMainBack';
-import Font1 from '../../components/common/Sogeting/SogetingFont1';
-import Btn1 from '../../components/common/Sogeting/button/NewPersonBtn';
-import FilterContainer from '../../components/common/Sogeting/FilterContainer';
-import FilterBody from '../../components/common/Sogeting/FilterBody';
-import ProfileContaine from '../../components/common/Sogeting/ProfileContainer';
-import Profile from '../../components/common/Sogeting/ProfileBody';
+import React from "react";
+import Back from "../../components/common/Sogeting/SogetingMainBack";
+import Font1 from "../../components/common/Sogeting/SogetingFont1";
+import Btn1 from "../../components/common/Sogeting/button/NewPersonBtn";
+import FilterContainer from "../../components/common/Sogeting/FilterContainer";
+import FilterBody from "../../components/common/Sogeting/FilterBody";
+import OnlineBox from "../../components/common/OnlineBtn";
+import BtnBox from "../../components/common/Sogeting/BtnBox";
+
+import {
+  ProfileContaine,
+  UnderBar,
+  Profile,
+  Online,
+  DetailProfile,
+  DetailText,
+} from "./SogaetinStyle";
 // import Button from '@mui/material/Button';
 
 const SoagetingFilter = () => {
-    return (
-        <div>
-            <Back>
-                <div
-                    style={{
-                        border: `solid 1px red`,
-                        height: '87vh'
-                    }}>
-                    <Font1
-                        style={{
-                            // border: 'solid 1px gold'
-                        }}>
-                        <p>소개팅
-                            <br/>
-                            추천</p>
-                    </Font1>
-                    <FilterContainer>
-                        <FilterBody></FilterBody>
-                    </FilterContainer>
-                </div>
-
-                <div
-                    style={{
-                        border: `solid 1px green`,
-                        height: '87vh',
-                        width: "25vw",
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
-                    <ProfileContaine>
-                        <Profile></Profile>
-                    </ProfileContaine>
-                    <ProfileContaine>
-                        <Profile></Profile>
-                    </ProfileContaine>
-                </div>
-
-                <div
-                    style={{
-                        // border: `solid 1px black`,
-                        height: '87vh',
-                        width: "13vw"
-                    }}>
-                    <Font1>
-                        <p>내 근처<br/>
-                            추천</p>
-                    </Font1>
-                    <Btn1/>
-                </div>
-
-                <div
-                    style={{
-                        border: `solid 1px orange`,
-                        height: '87vh',
-                        width: "50vw",
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
-                    <div>
-                        <ProfileContaine>
-                            <Profile></Profile>
-                        </ProfileContaine>
-                        <ProfileContaine>
-                            <Profile></Profile>
-                        </ProfileContaine>
-                    </div>
-                    <div>
-                        <ProfileContaine>
-                            <Profile></Profile>
-                        </ProfileContaine>
-                        <ProfileContaine>
-                            <Profile></Profile>
-                        </ProfileContaine>
-                    </div>
-                </div>
-            </Back>
+  return (
+    <div>
+      <Back>
+        <div
+          style={{
+            border: `solid 1px red`,
+            height: "87vh",
+          }}
+        >
+          <Font1
+            style={
+              {
+                // border: 'solid 1px gold'
+              }
+            }
+          >
+            <p>
+              소개팅
+              <br />
+              추천
+            </p>
+          </Font1>
+          <FilterContainer>
+            <FilterBody></FilterBody>
+          </FilterContainer>
         </div>
-    );
+
+        <div
+          style={{
+            // border: `solid 1px green`,
+            height: "87vh",
+            width: "25vw",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ProfileContaine>
+            <DetailProfile>
+              <DetailText>
+                <p>테버 / 29 / 188cm / 대전</p>
+                <br />
+                <p>저는 김치볶음밥을 좋아합니다.</p>
+                <br />
+                <p>#비흡연 #술조아 # ENFP</p>
+              </DetailText>
+            </DetailProfile>
+            <Profile>
+              <UnderBar>
+                <Online>
+                  <OnlineBox></OnlineBox>
+                </Online>
+                <BtnBox />
+              </UnderBar>
+            </Profile>
+          </ProfileContaine>
+
+          <ProfileContaine>
+            <DetailProfile></DetailProfile>
+            <Profile>
+              <UnderBar>
+                <Online>
+                  <OnlineBox></OnlineBox>
+                </Online>
+                <BtnBox></BtnBox>
+              </UnderBar>
+            </Profile>
+          </ProfileContaine>
+        </div>
+
+        <div
+          style={{
+            // border: `solid 1px black`,
+            height: "87vh",
+            width: "13vw",
+          }}
+        >
+          <Font1>
+            <p>
+              내 근처
+              <br />
+              추천
+            </p>
+          </Font1>
+          <Btn1 />
+        </div>
+
+        <div
+          style={{
+            // border: `solid 1px orange`,
+            height: "87vh",
+            width: "50vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <ProfileContaine>
+              <DetailProfile></DetailProfile>
+              <Profile>
+                <UnderBar>
+                  <Online>
+                    <OnlineBox></OnlineBox>
+                  </Online>
+                  <BtnBox />
+                </UnderBar>
+              </Profile>
+            </ProfileContaine>
+
+            <ProfileContaine>
+              <DetailProfile></DetailProfile>
+              <Profile>
+                <UnderBar>
+                  <Online>
+                    <OnlineBox></OnlineBox>
+                  </Online>
+                  <BtnBox />
+                </UnderBar>
+              </Profile>
+            </ProfileContaine>
+          </div>
+          <div>
+            <ProfileContaine>
+              <DetailProfile></DetailProfile>
+              <Profile>
+                <UnderBar>
+                  <Online>
+                    <OnlineBox></OnlineBox>
+                  </Online>
+                  <BtnBox />
+                </UnderBar>
+              </Profile>
+            </ProfileContaine>
+
+            <ProfileContaine>
+              <DetailProfile></DetailProfile>
+              <Profile>
+                <UnderBar>
+                  <Online>
+                    <OnlineBox></OnlineBox>
+                  </Online>
+                  <BtnBox />
+                </UnderBar>
+              </Profile>
+            </ProfileContaine>
+          </div>
+        </div>
+      </Back>
+    </div>
+  );
 };
 
 export default SoagetingFilter;
