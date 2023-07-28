b207f2f0752f76c3905bc6517b77bd23921454aa
노윤식 <gozldss12345@naver.com>
2023-07-28 22:17:38 +0900

    [S09P12B205-142] WIP:소개팅메인

--- Front-End/mannamanna/src/pages/Soagaeting/Soageting.tsx
+++ Front-End/mannamanna/src/pages/Soagaeting/Soageting.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import MacBookBox from '../../components/common/macbookBox';
+import unKnown from '../../asset/image/unknown.png';
 const Soageting = () => {
     return (
         <div>
-            소개팅 진행
+           <MacBookBox width="80vh" height="60vh" color1="#F8E3EA" color2="#ffffff" alignItems='center'>
+           <img src={unKnown} alt="Start" style={{ width: '100%', height: '90%' }} />
+           </MacBookBox>
         </div>
     );
 };
