74389b77e97a387b5af1940ee49008f1809a0fa1
qkdk <86948395+qkdk@users.noreply.github.com>
2023-07-30 17:17:27 +0900

    [S09P12B205-151] Feat: LoginSuccess, Failure 핸들러 구현

---
+++ Back-End/manna/src/main/java/com/ssafy/manna/global/jwt/LoginSuccessHandler.java
@@ -0,0 +1,40 @@
+package com.ssafy.manna.global.jwt;
+
+import com.ssafy.manna.member.repository.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+
+        String id = extractUsername(authentication);
+        String accessToken = jwtService.createAccessToken(id);
+        String refreshToken = jwtService.createRefreshToken();
+
+        jwtService.sendAccessTokenAndRefreshToken(response, accessToken, refreshToken);
+    }
+
+    private String extractUsername(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+}
