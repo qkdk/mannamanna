9c0f232ccab3c5d221f782d5841184fafa00ab4c
노윤식 <gozldss12345@naver.com>
2023-07-31 20:43:34 +0900

    [S09P12B205-177] Feat: Recoil 추가

--- Front-End/mannamanna/src/index.tsx
+++ Front-End/mannamanna/src/index.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
- import {Provider} from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { RecoilRoot } from 'recoil';
 const container =document.getElementById('root');
 if(!container) throw new Error('Failed to find the root element');
 const root=ReactDOM.createRoot(container);
 const queryClient =new QueryClient();
 
-const store= configureStore({reducer:rootReducer}); 
 
 root.render(
     <BrowserRouter>
     <LocalizationProvider dateAdapter={AdapterMoment}>
     <QueryClientProvider client={queryClient}>
-  <Provider store={store}>
-    <App />
-    </Provider>
+      <RecoilRoot>
+      <App />
+      </RecoilRoot>
    </QueryClientProvider>
     </LocalizationProvider>
     </BrowserRouter>
