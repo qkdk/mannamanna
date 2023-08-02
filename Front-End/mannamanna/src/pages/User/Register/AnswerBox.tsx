4be94814a2fd0e205bcb8da4b504d93686bc0bcb
Jaei <dobbykim0320@gmail.com>
2023-08-02 11:16:16 +0900

    [S09P12B205-216-recoil] Feat: 회원가입 정보 recoil로 mapping

--- Front-End/mannamanna/src/pages/User/Register/AnswerBox.tsx
+++ Front-End/mannamanna/src/pages/User/Register/AnswerBox.tsx
@@ -1,18 +1,17 @@
-// import React from "react";
-// import { Profile } from "../../../apis/Request/Request";
+import React from "react";
+import { useRecoilState } from "recoil";
 import {
   SmallInputBox,
   AnswerBox,
   Answer,
   SmallInput,
 } from "../Register/RegisterStyle";
-
+import { RegisterDataType, RegisterDataState } from "./RegisterState";
 interface QuestionProps {
   question: string;
   Type: string;
   Id: string;
   placeholder: string;
-  onChange: (value: string) => void; // Add this line
 }
 
 const Question: React.FC<QuestionProps> = ({
@@ -20,53 +19,34 @@ const Question: React.FC<QuestionProps> = ({
   Type,
   Id,
   placeholder,
-  onChange, // Add this line
 }) => {
-  // const [userName, setUserName] = React.useState<string>("");
-  // const [userTel, setUserTel] = React.useState<string>("");
-  // const [userId, setUserId] = React.useState<string>("");
-  // const [userPwd, setUserPwd] = React.useState<string>("");
-  // const [UserAddress, setUserAddress] = React.useState<string>("");
-  // console.log(userName);
-  // console.log(userTel);
-  // console.log(userId);
-  // console.log(userPwd);
-  // console.log(UserAddress);
+  const [registerData, setRegisterData] = useRecoilState(RegisterDataState);
 
-  // const enterInfo: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-  //   const { name, value } = event.target;
+  const { userName, userTel, userId, userPwd, userAddress } = registerData;
 
-  //   switch (name) {
-  //     case "userName":
-  //       setUserName(value);
-  //       break;
-  //     case "userTel":
-  //       setUserTel(value);
-  //       break;
-  //     case "userId":
-  //       setUserId(value);
-  //       break;
-  //     case "userPwd":
-  //       setUserPwd(value);
-  //       break;
-  //     case "UserAddress":
-  //       setUserAddress(value);
-  //       break;
-  //     default:
-  //       // 필요한 경우 다른 인풋 필드 처리
-  //       break;
-  //   }
-  // };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setRegisterData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(userName);
+    console.log(userTel);
+    console.log(userId);
+    console.log(userPwd);
+    console.log(userAddress);
+  };
 
   return (
     <SmallInputBox>
       <AnswerBox>
         <Answer>{question}</Answer>
         <SmallInput
-          onChange={(event) => onChange(event.target.value)} // Add this line
           type={Type}
           id={Id}
           placeholder={placeholder}
+          onChange={handleChange}
         />
       </AnswerBox>
     </SmallInputBox>
