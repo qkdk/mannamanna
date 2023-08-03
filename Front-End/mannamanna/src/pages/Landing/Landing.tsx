597daf16ddeffde374fec1e6eedebd212b815bf7
Jaei <dobbykim0320@gmail.com>
2023-08-03 21:04:29 +0900

    [S09P12B205-252] Design: 디자인 수정

--- Front-End/mannamanna/src/pages/Landing/Landing.tsx
+++ Front-End/mannamanna/src/pages/Landing/Landing.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  BigContainer,
-  CenteredDiv,
-  Content,
-  ContentBlue,
-  ContentLogo,
-  //   textandBtnContainer,
-} from "./LandingStyle";
-
+import { CenteredDiv, Content } from "./LandingStyle";
 import Logo from "../../components/common/Logo";
 import start from "../../asset/image/start.png";
 
@@ -23,38 +15,25 @@ const LandingPage = () => {
       <div style={{ height: "10vh", alignItems: "center" }}>
         <Logo />
       </div>
-
       <CenteredDiv
         style={{
-          border: "1px solid blue",
           height: "90vh",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <div>
-          <div style={{ border: "1px solid black" }}>
-            <BigContainer>
-              <ContentBlue>부담</ContentBlue>
-              <Content>없이 만나는</Content>
-            </BigContainer>
-            <Content>새로운 인연의 시작</Content>
-            <ContentLogo>맞나?만나!</ContentLogo>
-          </div>
-          <div>
-            <img
-              src={start}
-              alt="Start"
-              style={{
-                marginTop: "10vh",
-                maxWidth: "80vh",
-                maxHeight: "100vh",
-                cursor: "pointer",
-              }}
-              onClick={GoLogin}
-            />
-          </div>
-        </div>
+        <Content>
+          부담없이 만나는 <br />
+          새로운 인연의 시작
+          <br />
+          맞나?만나!
+        </Content>
+        <img
+          src={start}
+          alt="Start"
+          style={{ marginTop: "10vh", maxWidth: "80vh", maxHeight: "100vh" }}
+          onClick={GoLogin}
+        ></img>
       </CenteredDiv>
     </div>
   );
