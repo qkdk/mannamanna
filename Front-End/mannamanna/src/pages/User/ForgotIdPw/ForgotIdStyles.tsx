156c4b77e2af327961fd29a126139a80eeb39903
노윤식 <gozldss12345@naver.com>
2023-08-04 20:53:27 +0900

    [S09P12B205-267] Feat: 쪽지 페이지 생성 및 모달

--- Front-End/mannamanna/src/pages/User/ForgotIdPw/ForgotIdStyles.tsx
+++ Front-End/mannamanna/src/pages/User/ForgotIdPw/ForgotIdStyles.tsx
@@ -156,3 +156,63 @@ export const FindidModal = () => {
       </div>
     )
   }
+
+
+  // export const NoteModal = () => {
+
+  //   const [open, setOpen] = useRecoilState(RegisterModalAtom);
+  //   const handleOpen = () => setOpen(true);
+  //   const handleClose = () => setOpen(false);
+  //   const [message] =  useRecoilState(RegisterMessageAtom);
+  //   return(
+  //     <div style={{width:'30%'}}>
+  //       <Modal
+  //           open={open}
+  //           onClose={handleClose}
+  //           aria-labelledby="modal-modal-title"
+  //           aria-describedby="modal-modal-description"
+  //       >
+  //         <div style={{borderRadius:'5%',background:'white',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',width:'50%',height:'70%',flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center'}}>
+  //           <MacBookBox width="100%" height="100%" color1="#bcd3ff" color2="#ffffff" alignItems='center'>
+  //             <div style={{flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center',marginTop:'10vh'}}>
+  //              {message}  
+  //               <div style={{marginTop:'20vh'}}>
+  //               <MyPageButton onClick={handleClose} >확인</MyPageButton>
+  //               </div>
+  //             </div>
+  //           </MacBookBox>
+  //         </div>
+  //       </Modal>
+  //     </div>
+  //   )
+  // }
+
+
+  // export const LoveNoteModal = () => {
+
+  //   const [open, setOpen] = useRecoilState(RegisterModalAtom);
+  //   const handleOpen = () => setOpen(true);
+  //   const handleClose = () => setOpen(false);
+  //   const [message] =  useRecoilState(RegisterMessageAtom);
+  //   return(
+  //     <div style={{width:'30%'}}>
+  //       <Modal
+  //           open={open}
+  //           onClose={handleClose}
+  //           aria-labelledby="modal-modal-title"
+  //           aria-describedby="modal-modal-description"
+  //       >
+  //         <div style={{borderRadius:'5%',background:'white',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',width:'50%',height:'70%',flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center'}}>
+  //           <MacBookBox width="100%" height="100%" color1="#bcd3ff" color2="#ffffff" alignItems='center'>
+  //             <div style={{flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center',marginTop:'10vh'}}>
+  //              {message}  
+  //               <div style={{marginTop:'20vh'}}>
+  //               <MyPageButton onClick={handleClose} >확인</MyPageButton>
+  //               </div>
+  //             </div>
+  //           </MacBookBox>
+  //         </div>
+  //       </Modal>
+  //     </div>
+  //   )
+  // }
