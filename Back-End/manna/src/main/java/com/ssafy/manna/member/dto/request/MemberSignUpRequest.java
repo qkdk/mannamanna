c582a7ec318d7c0b3bb873720d995d5d2a9c11eb
박소현 <park.sohyun0430@gmail.com>
2023-08-04 23:44:20 +0900

    [S09P12B205-254] Fix: 이미지 multipart 수정

--- Back-End/manna/src/main/java/com/ssafy/manna/member/dto/request/MemberSignUpRequest.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/member/dto/request/MemberSignUpRequest.java
@@ -44,13 +44,13 @@ public class MemberSignUpRequest {
     private Double longitude;
 
     //회원 프로필 사진 3개
-    private MultipartFile profilePicture1;
+//    private MultipartFile profilePicture1;
     private Integer priority1;
 //
-    private MultipartFile profilePicture2;
+//    private MultipartFile profilePicture2;
     private Integer priority2;
 //
-    private MultipartFile profilePicture3;
+//    private MultipartFile profilePicture3;
     private Integer priority3;
 //
 
