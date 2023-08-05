f0a12491bcb903b876f0b34f25a6524e7bf6239c
노윤식 <gozldss12345@naver.com>
2023-08-05 19:41:35 +0900

    [S09P12B205-274] WIP: 쪽지 입력

--- Front-End/mannamanna/src/pages/Note/Note.tsx
+++ Front-End/mannamanna/src/pages/Note/Note.tsx
@@ -5,6 +5,9 @@ import { MyPageContainerBox } from '../User/MyPage/MyPageStyle';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { StyledButton } from '../User/Login/LoginStyle';
 import { FlexBox } from '../Reserve/ReserveStyle';
+import { SendNoteModalAtom } from '../../Recoil/State';
+import { useRecoilState } from 'recoil';
+import NoteModal from './../User/ForgotIdPw/ForgotIdStyles';
 
 const Note = () => {
   const navigate = useNavigate();
@@ -14,6 +17,13 @@ const Note = () => {
   const GoResponseNote = () => {
     navigate("/note");
   };
+
+  const [open, setOpen] = useRecoilState(SendNoteModalAtom);
+
+  const handleOpenNoteModal = () => {
+    setOpen(true);
+  };
+
     return (
         <div>
           <div style={{ height: "5vh" }}></div>
@@ -22,13 +32,21 @@ const Note = () => {
             </div>
             <div style={{ height: "80vh" }}>
               <MyPageContainerBox>
+                <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
                 <div style={{display:'flex'}}>
               <StyledButton onClick={GoResponseNote}>받은 쪽지</StyledButton>
-              <StyledButton onClick={GoRequestNote}>보낸 쪽지</StyledButton>    
+              <div>
+              <StyledButton onClick={GoRequestNote}>보낸 쪽지</StyledButton>       
+              </div>
+                </div>
+                <div>
+                <StyledButton onClick={handleOpenNoteModal}>쪽지보내기</StyledButton>  
+                </div>
                 </div>
               <Outlet></Outlet></MyPageContainerBox>
             </div>
           </BackBox>
+          <NoteModal></NoteModal>
         </div>
       );
 };
--- Front-End/mannamanna/src/pages/User/ForgotIdPw/ForgotIdStyles.tsx
+++ Front-End/mannamanna/src/pages/User/ForgotIdPw/ForgotIdStyles.tsx
@@ -1,12 +1,13 @@
 
 import { useRecoilState } from 'recoil';
 import { styled } from 'styled-components';
-import { ForgotIdErrorModalAtom, LoginErrorModalAtom, RegisterMessageAtom, RegisterModalAtom, findIdCheckIdAtom, findIdModalAtom, findPwModalAtom } from '../../../Recoil/State';
+import { ForgotIdErrorModalAtom, LoginErrorModalAtom, RegisterMessageAtom, RegisterModalAtom, SendNoteModalAtom, findIdCheckIdAtom, findIdModalAtom, findPwModalAtom } from '../../../Recoil/State';
 import MacBookBox from '../../../components/common/macbookBox';
 import { MyPageButton } from '../MyPage/MyPageStyles';
 import Modal from '@mui/material/Modal';
 import { useNavigate } from 'react-router-dom';
-
+import React, { useState, ChangeEvent } from 'react';
+import { TextField } from '@mui/material';
 
 export const FindidModal = () => {
 
@@ -156,37 +157,95 @@ export const FindidModal = () => {
       </div>
     )
   }
+  
+  const StyledModalContainer = styled.div`
+    width: 30%;
+  `;
+  
+  const StyledModalContent = styled.div`
+    border-radius: 5%;
+    background: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 50%;
+    height: 70%;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+  
+  const StyledFormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  `;
+  
+  const StyledButtonContainer = styled.div`
+    margin-top: 20px;
+  `;
+  
+  export const NoteModal = () => {
+    
+    const [open, setOpen] = useRecoilState(SendNoteModalAtom);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    const [sender, setSender] = useState('');
+    const [receiver, setReceiver] = useState('');
+    const [title, setTitle] = useState('');
+    const [contents, setContents] = useState('');
+  
+    const handleSubmit = () => {
+      console.log('Sender:', sender);
+      console.log('Receiver:', receiver);
+      console.log('Title:', title);
+      console.log('Contents:', contents);
+      handleClose();
+    };
+  
+    return (
+      <StyledModalContainer>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <StyledModalContent>
+          <MacBookBox width="100%" height="100%" color1="#bcd3ff" color2="#ffffff" alignItems='center'>
+            <StyledFormContainer>
+              
+              <TextField label="보내는 이" variant="outlined" value={sender} onChange={(e) => setSender(e.target.value)} />
+              <TextField label="받는 이" variant="outlined" value={receiver} onChange={(e) => setReceiver(e.target.value)} />
+              <TextField label="제목" variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)} />
+              <TextField
+                label="내용"
+                variant="outlined"
+                multiline
+                rows={4}
+                value={contents}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setContents(e.target.value)}
+              />
+                          <StyledButtonContainer>
+              <MyPageButton onClick={handleSubmit}>보내기</MyPageButton>
+              <MyPageButton onClick={handleClose}>취소</MyPageButton>
+            </StyledButtonContainer>
+            </StyledFormContainer>
+            </MacBookBox>
+          </StyledModalContent>
+        </Modal>
+      </StyledModalContainer>
+    );
+  }
 
 
-  // export const NoteModal = () => {
+  
 
-  //   const [open, setOpen] = useRecoilState(RegisterModalAtom);
-  //   const handleOpen = () => setOpen(true);
-  //   const handleClose = () => setOpen(false);
-  //   const [message] =  useRecoilState(RegisterMessageAtom);
-  //   return(
-  //     <div style={{width:'30%'}}>
-  //       <Modal
-  //           open={open}
-  //           onClose={handleClose}
-  //           aria-labelledby="modal-modal-title"
-  //           aria-describedby="modal-modal-description"
-  //       >
-  //         <div style={{borderRadius:'5%',background:'white',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',width:'50%',height:'70%',flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center'}}>
-  //           <MacBookBox width="100%" height="100%" color1="#bcd3ff" color2="#ffffff" alignItems='center'>
-  //             <div style={{flexDirection:'column',display:'flex',justifyContent:'center',alignItems:'center',marginTop:'10vh'}}>
-  //              {message}  
-  //               <div style={{marginTop:'20vh'}}>
-  //               <MyPageButton onClick={handleClose} >확인</MyPageButton>
-  //               </div>
-  //             </div>
-  //           </MacBookBox>
-  //         </div>
-  //       </Modal>
-  //     </div>
-  //   )
-  // }
-
+export default NoteModal;
 
   // export const LoveNoteModal = () => {
 
