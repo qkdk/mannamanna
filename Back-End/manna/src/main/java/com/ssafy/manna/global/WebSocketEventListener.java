2a80b78020a9961a961a9c99744a11e190058a43
안규보 <akb033134@gmail.com>
2023-08-07 15:26:56 +0900

    [S09P12B205-290] Feat: 유저 네임 추출기능 구현

--- Back-End/manna/src/main/java/com/ssafy/manna/global/WebSocketEventListener.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/global/WebSocketEventListener.java
@@ -1,22 +1,23 @@
 package com.ssafy.manna.global;
 
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketEventListener {
 
     @EventListener
-    public void handleWebSocketConnectionListener(SessionConnectedEvent event) {
-        log.info("세션연결 완료");
-    }
-
-    @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
-        log.info("세션연결 해제");
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
     }
 }
--- Back-End/manna/src/main/java/com/ssafy/manna/global/config/WebSocketConfig.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/global/config/WebSocketConfig.java
@@ -1,13 +1,24 @@
 package com.ssafy.manna.global.config;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -18,7 +29,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws").setAllowedOriginPatterns("*")
-            .withSockJS();
+        registry.addEndpoint("/api/ws").setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+                List<String> usernameHeaders = accessor.getNativeHeader("username");
+                if (usernameHeaders != null && !usernameHeaders.isEmpty()) {
+                    String username = usernameHeaders.get(0);
+                    // 서버에서 해당 헤더 값으로 원하는 동작 수행
+                    log.info("Username received from client: " + username);
+                    accessor.getSessionAttributes().put("username", username);
+                }
+                return message;
+            }
+        });
+
     }
 }
