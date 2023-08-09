f7aa323d15dd4a21de1913048ee50a1341a55ed8
박소현 <park.sohyun0430@gmail.com>
2023-08-09 10:37:20 +0900

    [S09P12B205-329] 소개팅 신청 쪽지 수락 시 스케줄 추가 기능 구현

--- Back-End/manna/src/main/java/com/ssafy/manna/schedule/domain/Schedule.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/schedule/domain/Schedule.java
@@ -6,16 +6,15 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorColumn
 @Inheritance(strategy = InheritanceType.JOINED)
+@AllArgsConstructor
 public class Schedule {
 
     @Id
