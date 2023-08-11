206e7e6967d3cc50ee42385a8e557a73a7eeddc1
park.sohyun0430 <park.sohyun0430@gmail.com>
2023-08-11 23:34:09 +0900

    [S09P12B205-375] Feat: 시도 구군 필터링 추천 장소 리스트 조회

--- Back-End/manna/src/main/java/com/ssafy/manna/schedule/controller/ReservePlaceController.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/schedule/controller/ReservePlaceController.java
@@ -1,15 +1,17 @@
 package com.ssafy.manna.schedule.controller;
 
+import com.ssafy.manna.global.util.ResponseTemplate;
+import com.ssafy.manna.schedule.domain.ReservePlace;
+import com.ssafy.manna.schedule.dto.request.ReservePlaceRequest;
 import com.ssafy.manna.schedule.service.ReservePlaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,17 +21,35 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class ReservePlaceController {
 
     private final ReservePlaceService reservePlaceService;
-    //예약하기 - 조회
-    @GetMapping("/{reserveId}")
-    public ResponseEntity<?> reservePlace(@PathVariable("reserveId") Integer reserveId ){
-        try {
-//            reservePlaceService.reserve(reserveId);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+//    //예약하기 - 조회
+//    @GetMapping("/{reserveId}")
+//    public ResponseEntity<?> reservePlace(@PathVariable("reserveId") Integer reserveId ){
+//        try {
+////            reservePlaceService.reserve(reserveId);
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+
+    //예약 - sido,gugun, category 골라서 return
+    @PostMapping("/placeList")
+    public ResponseEntity<?> getRecommendList(@RequestBody ReservePlaceRequest reservePlaceRequest){
+        ResponseTemplate<?> body;
+        try{
+            List<ReservePlace> recommendList =  reservePlaceService.getRecommendList(reservePlaceRequest);
+
+            body = ResponseTemplate.builder()
+                    .result(true)
+                    .msg("추천 리스트 조회 완료")
+                    .data(recommendList)
+                    .build();
+            return new ResponseEntity<>(body,HttpStatus.OK);
+
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
     }
-
-
-    //예약
 }
--- Back-End/manna/src/main/java/com/ssafy/manna/schedule/service/ReservePlaceService.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/schedule/service/ReservePlaceService.java
@@ -1,11 +1,17 @@
 package com.ssafy.manna.schedule.service;
 
 import com.ssafy.manna.schedule.domain.ReservePlace;
+import com.ssafy.manna.schedule.dto.request.ReservePlaceRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ReservePlaceService {
 
     //예약 정보 조회하기
     ReservePlace getPlaceInfo(Integer id) throws  Exception;
+
+    //추천 장소 조회하기 - sido,gugun,category
+    List<ReservePlace> getRecommendList(ReservePlaceRequest reservePlaceRequest) throws  Exception;
 }
