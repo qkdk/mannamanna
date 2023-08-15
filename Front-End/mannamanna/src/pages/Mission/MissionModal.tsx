140f1fac005cec2f69b65d24ddea2df96ff55994
Jaei <dobbykim0320@gmail.com>
2023-08-15 20:00:02 +0900

    [S09P12B205-415] Fix: Mission modal 오류 해결

--- Front-End/mannamanna/src/pages/Mission/MissionModal.tsx
+++ Front-End/mannamanna/src/pages/Mission/MissionModal.tsx
@@ -1,5 +1,4 @@
 import { Modal } from "@mui/material";
-import { useState } from "react";
 import { MissionCard } from "./MissionStyle";
 import { MyPageButton } from "../User/MyPage/MyPageStyles";
 import MacBookBox from "../../components/common/macbookBox";
@@ -12,52 +11,49 @@ import {
   ImageForm,
   EnterImageBtnBox,
 } from "../User/Register/ModalStyle";
+import { MissionCardAtom } from "../../Recoil/State";
+import { useRecoilState } from "recoil";
 
 interface MissionCardProps {
-  card: string;
   mission: string;
 }
 
-export const MissionCardBox = ({ mission, card }: MissionCardProps) => {
-  const [open, setOpen] = useState(false);
+export const MissionCardBox = ({ mission }: MissionCardProps) => {
+  const [open, setOpen] = useRecoilState(MissionCardAtom);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const handleSave = () => {
-    setOpen(false);
-  };
+
   return (
-    <MissionCard image={card} onClick={handleOpen}>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <MostBiggestBox>
-          <MacBookBox
-            width="100%"
-            height="100%"
-            color1="#bcd3ff"
-            color2="#ffffff"
-            alignItems="center"
-          >
-            <Contain>
-              {/* 이미지 입력받기 */}
-              <Container2>
-                <TitleBox>{mission}</TitleBox>
-                <ImageForm>
-                  <EnterMission1 title="User1" coment="User1" />
-                  <EnterMission2 title="User2" coment="User2" />
-                </ImageForm>
-                <EnterImageBtnBox>
-                  <MyPageButton onClick={handleClose}>확인</MyPageButton>
-                  <MyPageButton onClick={handleClose}>취소</MyPageButton>
-                </EnterImageBtnBox>
-              </Container2>
-            </Contain>
-          </MacBookBox>
-        </MostBiggestBox>
-      </Modal>
-    </MissionCard>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <MostBiggestBox>
+        <MacBookBox
+          width="100%"
+          height="100%"
+          color1="#bcd3ff"
+          color2="#ffffff"
+          alignItems="center"
+        >
+          <Contain>
+            {/* 이미지 입력받기 */}
+            <Container2>
+              <TitleBox>{mission}</TitleBox>
+              <ImageForm>
+                <EnterMission1 title="User1" coment="User1" />
+                <EnterMission2 title="User2" coment="User2" />
+              </ImageForm>
+              <EnterImageBtnBox>
+                <MyPageButton onClick={handleClose}>확인</MyPageButton>
+                <MyPageButton onClick={handleClose}>취소</MyPageButton>
+              </EnterImageBtnBox>
+            </Container2>
+          </Contain>
+        </MacBookBox>
+      </MostBiggestBox>
+    </Modal>
   );
 };
