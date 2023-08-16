ceb65f0d98986f56545d6a88045743960284c96f
안규보 <akb033134@gmail.com>
2023-08-16 16:16:04 +0900

    [S09P12B205-409] Feat: 예약기능 구현

--- Front-End/mannamanna/src/pages/Reserve/ReservePlaceComp.tsx
+++ Front-End/mannamanna/src/pages/Reserve/ReservePlaceComp.tsx
@@ -8,6 +8,16 @@ import {
 import {useState} from "react";
 import styled from "styled-components";
 import axios from "axios";
+import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import dayjs, {Dayjs} from 'dayjs';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import MacBookBox from "../../components/common/macbookBox";
+
+const InnerBox = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 1);
+`
 
 const PlaceContainer = styled.div`
   height: 100%;
@@ -92,7 +102,7 @@ const ModalContainer = styled.div<ModalContainerProps>`
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
-  z-index: 9999;
+  z-index: 2;
 `;
 
 const ModalContent = styled.div`
@@ -104,7 +114,85 @@ const ModalContent = styled.div`
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
 `;
 
-const insertReservation = (ReservationOfflineRequest: IReservationOfflineRequest) => {
+
+
+const DateTimeBox = styled.div`
+  width: 70%;
+  padding-left: 15%;
+  padding-right: 15%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: 10%;
+`
+
+const UserDetailBox = styled.div`
+  width: 70%;
+  padding-right: 15%;
+  padding-left: 15%;
+  height: 40%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const ModalBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const IDBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const TitleBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 22px;
+  padding-bottom: 3vh;
+  white-space: nowrap; /* 텍스트가 줄바꿈되지 않도록 설정 */
+  overflow: hidden; /* 넘치는 내용을 숨김 */
+  text-overflow: ellipsis; /* 넘치는 내용에 ... 표시 */
+`
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 10%;
+`
+
+const ReserveFetchButton = styled.button`
+  background: none;
+  padding: 0;
+  outline: none;
+  color: inherit;
+  font: inherit;
+  cursor: pointer;
+  -webkit-appearance: none; /* Safari와 Chrome */
+  -moz-appearance: none; /* Firefox */
+  appearance: none; /* 모든 브라우저에서 기본 버튼 스타일 제거 */
+  border: none;
+  border-radius: 5px;
+  font-size: 15px;
+  width: 50%;
+  height: 100%;
+  background-color: #bcd3ff;
+`
+
+const insertReservation = (ReservationOfflineRequest: {
+    femaleId: string;
+    date: string;
+    maleId: string;
+    reserveAddressId: number | undefined
+}) => {
     return axios({
         method: "POST",
         url: "https://i9b205.p.ssafy.io/api/schedule/offline/insert",
@@ -114,34 +202,85 @@ const insertReservation = (ReservationOfflineRequest: IReservationOfflineRequest
 }
 
 const ReservePlaceComp = (props: IReservePlaceProps) => {
+
     function initialState() {
         return [
-            <PlacePageButton key={1} onClick={() => {ItemList(1)}}>{1}</PlacePageButton>,
-            <PlacePageButton key={2} onClick={() => {ItemList(2)}}>{2}</PlacePageButton>,
-            <PlacePageButton key={3} onClick={() => {ItemList(3)}}>{3}</PlacePageButton>,
-            <PlacePageButton key={4} onClick={() => {ItemList(4)}}>{4}</PlacePageButton>,
-            <PlacePageButton key={5} onClick={() => {ItemList(5)}}>{5}</PlacePageButton>];
+            <PlacePageButton key={1} onClick={() => {
+                ItemList(1)
+            }}>{1}</PlacePageButton>,
+            <PlacePageButton key={2} onClick={() => {
+                ItemList(2)
+            }}>{2}</PlacePageButton>,
+            <PlacePageButton key={3} onClick={() => {
+                ItemList(3)
+            }}>{3}</PlacePageButton>,
+            <PlacePageButton key={4} onClick={() => {
+                ItemList(4)
+            }}>{4}</PlacePageButton>,
+            <PlacePageButton key={5} onClick={() => {
+                ItemList(5)
+            }}>{5}</PlacePageButton>];
     }
 
     const [index, setIndex] = useState(props.index);
     const [data, setData] = useState(props.data.slice(index * 10, index * 10 + 10));
     const [itemElements, setItemElements] = useState<any>(initialState())
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = () => {
+    const [currentShop, setCurrentShop] = useState<IReservePlace>();
+    const openModal = (item:IReservePlace) => {
+        setCurrentShop(item);
         setIsModalOpen(true);
     };
     const closeModal = () => {
         setIsModalOpen(false);
     };
     const [placeComponent, setPlaceComponent] = useState<any>(makePlaceComponent(data));
+    const [date, setDate] = useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
 
-    const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+    const Modal: React.FC<ModalProps> = ({isOpen, onClose}) => {
         return (
-            <ModalContainer isOpen={isOpen}>
-                <ModalContent>
-                    <span className="close" onClick={onClose}>&times;</span>
-                    <p>This is a modal!</p>
-                </ModalContent>
+            <ModalContainer isOpen={isOpen} onClick={closeModal}>
+                <MacBookBox width={"30vw"} height={"70vh"} color1={"#bcd3ff"} color2={"#ffffff"} alignItems={"center"}>
+                    <InnerBox>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <ModalBox>
+                                <DateTimeBox>
+                                    <TitleBox>
+                                        장소 : {currentShop?.name}
+                                    </TitleBox>
+                                    <TitleBox>
+                                        소개팅시간을 선택하세요
+                                    </TitleBox>
+                                    <DateTimePicker
+                                        label="예약 날짜 선택"
+                                        value={date}
+                                        onAccept={(newValue) => setDate(newValue)}
+                                    />
+                                </DateTimeBox>
+                                <UserDetailBox>
+                                    <IDBox>
+                                        남성 아이디: {props.maleId}
+                                    </IDBox>
+                                    <br/>
+                                    <IDBox>
+                                        💕
+                                    </IDBox>
+                                    <br/>
+                                    <IDBox>
+                                        여성 아이디: {props.femaleId}
+                                    </IDBox>
+                                </UserDetailBox>
+                                <ButtonBox>
+                                    <ReserveFetchButton onClick={() => {
+                                        insertReservation(makeRequestJSON(currentShop)).then(() => alert("예약 성공"));
+                                    }}>
+                                        예약하기
+                                    </ReserveFetchButton>
+                                </ButtonBox>
+                            </ModalBox>
+                        </LocalizationProvider>
+                    </InnerBox>
+                </MacBookBox>
             </ModalContainer>
         );
     };
@@ -178,13 +317,13 @@ const ReservePlaceComp = (props: IReservePlaceProps) => {
         setItemElements(itemElements);
     }
 
-    function makeRequestJSON(item: IReservePlace) {
-        const now = new Date();
+    function makeRequestJSON(item: IReservePlace | undefined) {
+        const formattedDate = `${date?.format('YYYY년 MM월 DD일 HH시 mm분')}`;
         return {
             femaleId: props.femaleId,
             maleId: props.maleId,
-            date: `${now.getFullYear()}년 ${String(now.getMonth()).padStart(2, '0')}월 ${String(now.getDay()).padStart(2, '0')}일 ${String(now.getHours()).padStart(2, '0')}시 ${String(now.getMinutes()).padStart(2, '0')}분`,
-            reserveAddressId: item.id
+            date: formattedDate,
+            reserveAddressId: item?.id
         };
     }
 
@@ -196,11 +335,7 @@ const ReservePlaceComp = (props: IReservePlaceProps) => {
                         {item.name}
                     </PlaceNameBox>
                     <ReserveButton
-                        //     onClick={() => insertReservation(makeRequestJSON(item))
-                        //         .then((response) => alert(response.data.msg))
-                        //         .catch(() => alert("스케쥴 등록에 실패하였습니다."))
-                        // }
-                        onClick={openModal}
+                        onClick={() => openModal(item)}
                     >
                         ️💖예약하기
                     </ReserveButton>
@@ -225,7 +360,7 @@ const ReservePlaceComp = (props: IReservePlaceProps) => {
                     ▶︎
                 </PlacePageButton>
             </PlacePagingButtonBox>
-            <Modal isOpen={isModalOpen} onClose={closeModal} />
+            <Modal isOpen={isModalOpen} onClose={closeModal}/>
         </PlaceContainer>
     )
 }
