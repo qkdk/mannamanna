b45d0c35d57c78f32646ee3bd2fff3f0fcc954bc
qkdk <86948395+qkdk@users.noreply.github.com>
2023-08-17 23:15:10 +0900

    Refactor: 소켓 js 용 코드 변경

--- Back-End/manna/src/main/java/com/ssafy/manna/global/handler/StompHandler.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/global/handler/StompHandler.java
@@ -7,11 +7,14 @@ import com.ssafy.manna.messenger.domain.RedisChat;
 import com.ssafy.manna.messenger.domain.RedisChatHistory;
 import com.ssafy.manna.messenger.dto.ChatMessage;
 import com.ssafy.manna.messenger.repository.ChatRepository;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
@@ -31,6 +34,22 @@ public class StompHandler implements ChannelInterceptor {
     // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.CONNECT.equals(header.getCommand())) {
+            //connect라면 name값을 꺼내서 sessionAttributes에 넣기.
+            Map<String, Object> attributes = header.getSessionAttributes();
+            attributes.put("userId", header.getFirstNativeHeader("userId"));
+            attributes.put("userName", header.getFirstNativeHeader("userName"));
+            attributes.put("gender", header.getFirstNativeHeader("gender"));
+            attributes.put("token", header.getFirstNativeHeader("token"));
+            header.setSessionAttributes(attributes);
+
+//          혹시 한글 안나오면 이거 주석 해제  하세요
+//            header.setNativeHeader("Content-Type", MimeTypeUtils.APPLICATION_JSON_VALUE);
+//            header.setNativeHeader("charset", StandardCharsets.UTF_8.name());
+        }
+
         try {
             ChatMessage chatMessage = encodeChatMessaege(message);
             log.info("TALK메시지 수신 " + chatMessage.getSenderId() + " : " + chatMessage.getRoomId());
