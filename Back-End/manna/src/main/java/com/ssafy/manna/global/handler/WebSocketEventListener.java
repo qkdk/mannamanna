a19b021afd7b4ce628251ad3b5cc7bfd5de92570
qkdk <86948395+qkdk@users.noreply.github.com>
2023-08-17 23:41:05 +0900

    Fix: 소켓 js 용 코드 변경2

--- Back-End/manna/src/main/java/com/ssafy/manna/global/handler/WebSocketEventListener.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/global/handler/WebSocketEventListener.java
@@ -19,23 +19,23 @@ public class WebSocketEventListener {
 
     private final RedisSessionRepository redisSessionRepository;
 
-    @EventListener
-    public void handleWebSocketConnectListener(SessionConnectEvent event) {
-        log.info("도커 커넥션 핸들러 작동");
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        try {
-            String userName = headerAccessor.getNativeHeader(SOCKET_HEADER_USER_NAME.getValue())
-                    .get(0);
-            String gender = headerAccessor.getNativeHeader(SOCKET_HEADER_GENDER.getValue()).get(0);
-            String userId = headerAccessor.getNativeHeader(SOCKET_HEADER_USER_ID.getValue()).get(0);
-            headerAccessor.getSessionAttributes().put(SOCKET_HEADER_USER_ID.getValue(), userId);
-
-            saveSession(userName, gender, userId);
-        } catch (Exception e) {
-
-        }
-
-    }
+//    @EventListener
+//    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+//        log.info("도커 커넥션 핸들러 작동");
+//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+//        try {
+//            String userName = headerAccessor.getNativeHeader(SOCKET_HEADER_USER_NAME.getValue())
+//                    .get(0);
+//            String gender = headerAccessor.getNativeHeader(SOCKET_HEADER_GENDER.getValue()).get(0);
+//            String userId = headerAccessor.getNativeHeader(SOCKET_HEADER_USER_ID.getValue()).get(0);
+//            headerAccessor.getSessionAttributes().put(SOCKET_HEADER_USER_ID.getValue(), userId);
+//
+//            saveSession(userName, gender, userId);
+//        } catch (Exception e) {
+//
+//        }
+//
+//    }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
