5199d399516c34e6aed6f6f46ee3d137923ad2fc
최제혁 <cjh970324@naver.com>
2023-08-17 14:56:34 +0900

    Fix: 소개팅 시작 시 저장 구현

--- Back-End/manna/src/main/java/com/ssafy/manna/sogaeting/dto/request/SogaetingStartRequest.java
+++ Back-End/manna/src/main/java/com/ssafy/manna/sogaeting/dto/request/SogaetingStartRequest.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class SogaetingStartRequest {
 
     // 스케줄 아이디와 동일
-    private int id;
+    private int sogaetingId;
     // 여자 아이디
     private String femaleId;
     // 남자 아이디
